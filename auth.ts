b1b3e44e7b557cba23fc3118436510000807ac30
alexandre corrette <alexandrecorrette@gmail.com>
2024-10-17 11:33:35 +0200

    authentication added

---
+++ auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import type { User } from './src/app/lib/definitions';
+import bcrypt from "bcrypt";
+
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        const res = await fetch(`http://localhost:8000/users/${email}`);
+        const user = res.json();
+        return user;
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+              const parsedCredentials = z
+                .object({ email: z.string().email(), password: z.string().min(6) })
+                .safeParse(credentials);
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                  }
+           
+                  return null;
+            },
+          }),],
+});
--- src/app/ui/header/account.tsx
+++ src/app/ui/header/account.tsx
@@ -2,14 +2,14 @@ import { UserCircleIcon, BellIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { User } from "../../lib/definitions";
 
-export default function account({ user, }: { user: User | null }) {
+export default function account({ user, }: { user: User}) {
     
     return (
         <div className="flex flex-row justify-center items-center w-full">
             <div className="flex flex-row justify-center items-center">
 
             <BellIcon className="size-6 justify-self-end text-yellow-300 "/>
-            <Link href="/user/{id}/account" className="flex flex-row items-center mx-2"><UserCircleIcon className="size-6 mx-2"/>{user ? user.firstName + ' ' + user.lastName : null } </Link>
+            <Link href="/user/{id}/account" className="flex flex-row items-center mx-2"><UserCircleIcon className="size-6 mx-2"/>{user.firstName + ' ' + user.lastName } </Link>
             </div>
 
         </div>
